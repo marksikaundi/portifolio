92227226ee5033ff64f5814e7e69a4bc89435848
marksikaundi <emmanuelmark117@gmail.com>
2023-03-24 19:53:53 +0200

    updates articles

--- src/components/content/pages/articles/articles.js
+++ src/components/content/pages/articles/articles.js
@@ -3,7 +3,7 @@ export const articles = [
     title: 'What does Express solve for us?',
     image: require('../../../../assets/articles/express_article.png'),
     createdAt: 'March 17, 2023',
-    link: 'https://medium.com/@virag-ky/what-does-express-solve-for-us-b0b06e274a36',
+    link: 'https://medium.com/@emmanuelmark117/what-does-expressjs-solve-for-both-frontend-and-backend-development-ab76e049f676',
     likes: 0,
   },
 ];
